cbb33d1122c8991bcb81dcca2acc420e50b0d139
alexisantoniou <alexis.antoniou89@gmail.com>
2026-06-08 16:53:04 +0300

    learn variables - strings numbers booleans

---
+++ index.js
@@ -0,0 +1,22 @@
+// let trainerName = "Alexis";
+// let numberOfClients = 10;
+// let isActicve = true;
+
+// console.log(trainerName);
+// console.log(numberOfClients);
+// console.log(isActicve);
+
+// let trainerName = "Alexis";
+// let numberOfClients = 10;
+// let priceSession = 35;
+// let totalEarnings = numberOfClients * priceSession;
+
+// console.log("Trainer:", trainerName);
+// console.log("Total Earnings:", totalEarnings);
+
+const appName = "GymnasticsApp";
+let numberOfClients = 10;
+numberOfClients = 12;
+
+console.log("App:", appName);
+console.log("Clients now:", numberOfClients);
